6e2aba72cfc05014e20fdaea4de60401be29314c
Seth Price <sethrprice@me.com>
2024-11-11 08:35:01 +0000

    main file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-
 mod wave;
 use wave::Wave;
 
+mod phasevocoder;
+use phasevocoder::PhaseVocoder;
+
+use std::error::Error;
+
+const FRAME_SIZE: usize = 1024;
 
 fn main() {
-    let wave = Wave::try_from("test.wav");
-    println!("Wave = {wave:?}")
+    if let Err(e) = run() {
+        eprintln!("Application error: {e}")
+    }
+}
+
+// TODO fix the errors!
+fn run() -> Result<(), Box<dyn Error>> {
+    let wave = Wave::try_from("test.wav")?;
+    println!("{wave:?}");
+    let phase_vocoder = PhaseVocoder::new(&wave, FRAME_SIZE);
+    let new_wave = phase_vocoder.shift_signal(1);
+    println!("{new_wave:?}");
+    new_wave.write_to_wav_file("test_out.wav")?;
+    Ok(())
 }
