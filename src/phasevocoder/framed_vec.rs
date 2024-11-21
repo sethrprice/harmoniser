4305fa4337522e994862c1abe0824f027287eb34
Seth Price <sethrprice@me.com>
2024-11-21 20:52:14 +0000

    improved module speed by making contiguous vecs

--- src/phasevocoder/framed_vec.rs
+++ src/phasevocoder/framed_vec.rs
@@ -6,102 +6,75 @@ pub struct FramedVec<T> {
     vec: Vec<T>,
     hop_size: usize,
     frame_size: usize,
-    indices: Vec<(usize, usize)>,
-}
-
-pub struct FrameIter<'a, T> {
-    vec: &'a [T],
-    indices: std::slice::Iter<'a, (usize, usize)>,
+    n_frames: Option<usize>,
 }
 
 impl<T: Clone + Num> FramedVec<T> {
-    pub fn new(vec: Vec<T>, frame_size: usize, hop_size: usize) -> Self {
-        // calculate the number of frames and the length of the vector + the padding
-        let num_frames = (vec.len() + hop_size - 1) / hop_size;
-        let padded_length = (num_frames.saturating_sub(1)) * hop_size + frame_size;
+    pub fn new(samples: Vec<T>, frame_size: usize, hop_size: usize) -> Self {
+        let n_frames = (samples.len() + hop_size - 1) / hop_size;
+        let mut framed_vec: Vec<T> = Vec::with_capacity(n_frames * frame_size);
 
-        // pad the vector so we always frame all our samples
-        let mut padded_vec = vec.clone();
-        padded_vec.resize(padded_length, T::zero());
+        for i in 0..n_frames {
+            let start = i * hop_size;
+            let end = start + frame_size;
 
-        let indices: Vec<(usize, usize)> = (0..num_frames)
-            .map(|i| (i * hop_size, i * hop_size + frame_size))
-            .collect();
+            if end <= samples.len() {
+                let frame = &samples[start..end];
+                framed_vec.extend_from_slice(frame);
+            } else {
+                // handle padding
+                let frame = &samples[start..];
+                framed_vec.extend_from_slice(frame);
+                framed_vec.extend(std::iter::repeat(T::zero()).take(frame_size - frame.len()));
+            }
+        }
 
         Self {
-            vec: padded_vec,
+            vec: framed_vec,
             hop_size,
             frame_size,
-            indices,
+            n_frames: Some(n_frames),
         }
     }
 
-    // TODO use Option instead of an empty vec
-    pub fn new_empty_non_overlapped(length: usize, frame_size: usize) -> Self {
-        let vec = Vec::with_capacity(length);
-        let indices = vec![];
+    pub fn with_capacity(capacity: usize, frame_size: usize, hop_size: usize) -> Self {
         Self {
-            vec,
-            hop_size: frame_size,
+            vec: Vec::with_capacity(capacity),
             frame_size,
-            indices,
+            hop_size,
+            n_frames: None,
         }
     }
 
-    pub fn push(&mut self, frame: Vec<T>) {
-        for element in frame {
-            self.vec.push(element);
-        }
+    pub fn with_capacity_like(other: &Self) -> Self {
+        Self::with_capacity(other.len(), other.frame_size, other.hop_size)
+    }
+
+    pub fn frame(&self, frame_idx: usize) -> Option<&[T]> {
+        let start = frame_idx * self.frame_size;
+        let end = start + self.frame_size;
+        self.vec.get(start..end)
+    }
+
+    pub fn extend_from_slice(&mut self, slice: &[T]) {
+        self.vec.extend_from_slice(slice);
+        self.n_frames = Some((self.vec.len() + self.hop_size - 1) / self.hop_size);
     }
 
     pub fn len(&self) -> usize {
         self.vec.len()
     }
 
-    pub fn iter(&self) -> FrameIter<T> {
-        FrameIter {
-            vec: &self.vec,
-            indices: self.indices.iter(),
-        }
-    }
-
-    pub fn hop(&self) -> usize {
-        self.hop_size
-    }
-
     pub fn frame_size(&self) -> usize {
         self.frame_size
     }
 
-    pub fn update_indices(&mut self) {
-        let num_frames = (self.vec.len() + self.hop_size - 1) / self.hop_size;
-        let padded_length = (num_frames - 1) * self.hop_size + self.frame_size;
-
-        self.vec.resize(padded_length, T::zero());
-
-        let indices: Vec<(usize, usize)> = (0..num_frames)
-            .map(|i| (i * self.hop_size, i * self.hop_size + self.frame_size))
-            .collect();
-
-        self.indices = indices;
-    }
-
     pub fn number_of_frames(&self) -> usize {
-        self.indices.len()
+        self.n_frames.unwrap_or(0)
     }
 
     #[cfg(test)]
     pub fn clone_vec(&self) -> Vec<T> {
         self.vec.clone()
-    }
-}
-
-impl<'a, T> Iterator for FrameIter<'a, T> {
-    type Item = &'a [T];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.indices
-            .next()
-            .map(|&(start, end)| &self.vec[start..end])
     }
 }
--- src/phasevocoder/mod.rs
+++ src/phasevocoder/mod.rs
@@ -6,6 +6,7 @@ use rustfft::{algorithm::Radix4, num_complex::Complex, Fft, FftDirection};
 use std::f32::consts::PI;
 mod framed_vec;
 use framed_vec::FramedVec;
+use std::time::Instant;
 
 pub struct PhaseVocoder {
     frame_size: usize,
@@ -38,33 +39,43 @@ impl PhaseVocoder {
         let hop_out = (alpha * (self.hop_size as f32)).round() as usize;
 
         // Analysis
-        println!("start analysis...");
+        let now_a = Instant::now();
         let frames = generate_frames(waveform.to_vec(), self.frame_size, self.hop_size);
         println!("number of samples = {}", frames.len());
-        let fft_frames = fft(frames);
-        println!("end analysis...");
+        println!("number of frames = {}", frames.number_of_frames());
+        let now_fft = Instant::now();
+        let fft_frames = fft(frames, self.frame_size, self.hop_size);
+        let elapsed_fft = now_fft.elapsed();
+        println!("FFT took {elapsed_fft:.2?}");
+        let elapsed_a = now_a.elapsed();
+        println!("analysis took {elapsed_a:.2?}");
         // End analysis
 
         // Processing
-        println!("start processing...");
+        let now_p = Instant::now();
         let phase_differences = get_phase_difference(&fft_frames);
-        let corrected_phase_differences = correct_phase_diffs(phase_differences, self.hop_size);
-        let true_frequencies = get_true_frequency(corrected_phase_differences, self.hop_size);
-        let final_phases = get_cumulative_phases(true_frequencies, hop_out);
-        println!("end processing");
+        let corrected_phase_differences = correct_phase_diffs(&phase_differences, self.hop_size);
+        let true_frequencies = get_true_frequency(&corrected_phase_differences, self.hop_size);
+        let final_phases = get_cumulative_phases(&true_frequencies, hop_out);
+        let elapsed_p = now_p.elapsed();
+        println!("processing took {elapsed_p:.2?}");
         // End processing
 
         // Synthesis
-        println!("start synthesis...");
+        let now_s = Instant::now();
         let output_frames = inverse_fft(&fft_frames, &final_phases);
-        let overlapped_waveform = overlap_add_frames(output_frames, hop_out, true);
-        println!("end synthesis...");
+        let elapsed_ifft = now_s.elapsed();
+        println!("IFFT took {elapsed_ifft:.2?}");
+        let overlapped_waveform = overlap_add_frames(&output_frames, hop_out, true);
+        let elapsed_s = now_s.elapsed();
+        println!("synthesis took {elapsed_s:.2?}");
         // End synthesis
 
         // Resampling
-        println!("start resampling...");
+        let now_r = Instant::now();
         let output_waveform = resample(overlapped_waveform, alpha);
-        println!("end resampling...");
+        let elapsed_r = now_r.elapsed();
+        println!("resampling took {elapsed_r:.2?}");
         // End resampling
 
         return output_waveform;
@@ -108,55 +119,60 @@ fn generate_frames(samples: Vec<f32>, frame_size: usize, hop_size: usize) -> Fra
     FramedVec::new(samples, frame_size, hop_size)
 }
 
-pub fn generate_hanning_window(length: usize) -> Vec<f32> {
+pub fn generate_hanning_window(length: usize, hop: usize, normalise: bool) -> Vec<f32> {
+    // normalisation of window compensates for overlap adding
+    let normalisation = if normalise {
+        (((length / hop) / 2) as f32).sqrt()
+    } else {
+        1.
+    };
+
     // generate a symmetrical Hanning window
-    let window: Vec<f32> = hanning_iter(length * 2 + 1)
+    hanning_iter(length * 2 + 1)
         .skip(1)
         .step_by(2)
-        .map(|v| v as f32)
-        .collect();
-    return window;
+        .map(|v| (v as f32) / normalisation)
+        .collect()
 }
 
-pub fn apply_hanning_window(frame: &[f32], hop: usize) -> Vec<f32> {
-    // normalisation of window compensates for overlap adding
-    let normalisation = (((frame.len() / hop) / 2) as f32).sqrt();
+pub fn apply_hanning_window(frame: &[f32], window: &[f32]) -> Vec<f32> {
+    assert_eq!(frame.len(), window.len());
 
-    let window = generate_hanning_window(frame.len());
-
-    let windowed_frame = frame
+    frame
         .iter()
         .zip(window.iter())
-        .map(|(data, w)| data * w / normalisation)
-        .collect();
-
-    return windowed_frame;
+        .map(|(x, w)| x * w)
+        .collect()
 }
 
-fn apply_fft_to_frame<T>(frame: &[T], direction: FftDirection) -> Vec<Complex<f32>>
+fn apply_fft_to_frame<T>(frame: &[T], fft_engine: &dyn Fft<f32>) -> Vec<Complex<f32>>
 where
     T: Into<Complex<f32>> + Copy,
 {
-    let fft_size = frame.len();
-    let fft = Radix4::new(fft_size, direction);
     let mut buffer: Vec<Complex<f32>> = frame.iter().map(|&x| x.into()).collect();
-    fft.process(&mut buffer);
+    fft_engine.process(&mut buffer);
     return buffer;
 }
 
-fn fft(frames: FramedVec<f32>) -> FramedVec<Complex<f32>> {
+fn fft(frames: FramedVec<f32>, frame_size: usize, hop: usize) -> FramedVec<Complex<f32>> {
     let mut fft_frames: FramedVec<Complex<f32>> =
-        FramedVec::new_empty_non_overlapped(frames.len(), frames.frame_size());
+        FramedVec::with_capacity(frames.len(), frame_size, frame_size);
 
-    for frame in frames.iter() {
-        let windowed_frame = apply_hanning_window(frame, frames.hop());
-        let fft_frame = apply_fft_to_frame(&windowed_frame, FftDirection::Forward);
+    let window = generate_hanning_window(frame_size, hop, true);
+    let default: Vec<f32> = vec![0.; frame_size];
+    let number_of_frames = frames.number_of_frames();
 
-        fft_frames.push(fft_frame);
+    let fft_engine = Radix4::new(frame_size, FftDirection::Forward);
+
+    // TODO does writing this functional style improve performance?
+    for i in 0..number_of_frames {
+        let frame = frames.frame(i).unwrap_or(&default);
+        let windowed_frame = apply_hanning_window(frame, &window);
+        let fft_frame = apply_fft_to_frame(&windowed_frame, &fft_engine);
+
+        fft_frames.extend_from_slice(&fft_frame);
     }
-    fft_frames.update_indices();
 
-    // hop is now frame size because fft spectrum has no overlapping
     return fft_frames;
 }
 
@@ -167,20 +183,29 @@ fn fft(frames: FramedVec<f32>) -> FramedVec<Complex<f32>> {
 // we now have fixed frequency bins, but we want the true frequencies (which in general lie between bins)
 // to do this we start by getting the phase differences between frames
 fn get_phase_difference(fft_spectrum: &FramedVec<Complex<f32>>) -> FramedVec<f32> {
-    let mut phase_diffs: Vec<f32> = Vec::with_capacity(fft_spectrum.len());
+    let mut phase_diffs: FramedVec<f32> = FramedVec::with_capacity(
+        fft_spectrum.len(),
+        fft_spectrum.frame_size(),
+        fft_spectrum.frame_size(),
+    );
+
+    let number_of_frames = fft_spectrum.number_of_frames();
 
     // first frame needs to be done manually
     let first_frame_diffs: Vec<f32> = fft_spectrum
+        .frame(0)
+        .expect("frame index out of bounds")
         .iter()
-        .take(1)
-        .flat_map(|frame| frame.iter().map(|c| c.arg()))
+        .map(|c| c.arg())
         .collect();
-    for diff in first_frame_diffs {
-        phase_diffs.push(diff);
-    }
+    phase_diffs.extend_from_slice(&first_frame_diffs);
 
     // iterate over all consecutive frames and take the phase difference between them
-    for (prev_frame, curr_frame) in fft_spectrum.iter().zip(fft_spectrum.iter().skip(1)) {
+    for i in 0..(number_of_frames - 1) {
+        let prev_frame = fft_spectrum.frame(i).expect("frame index out of bounds");
+        let curr_frame = fft_spectrum
+            .frame(i + 1)
+            .expect("frame index out of bounds");
         let frame_diffs: Vec<f32> = prev_frame
             .iter()
             .zip(curr_frame.iter())
@@ -192,25 +217,21 @@ fn get_phase_difference(fft_spectrum: &FramedVec<Complex<f32>>) -> FramedVec<f32
                 phase_diff
             })
             .collect();
-
-        for diff in frame_diffs {
-            phase_diffs.push(diff);
-        }
+        phase_diffs.extend_from_slice(&frame_diffs);
     }
 
-    FramedVec::new(
-        phase_diffs,
-        fft_spectrum.frame_size(),
-        fft_spectrum.frame_size(),
-    )
+    return phase_diffs;
 }
 
-fn correct_phase_diffs(phase_diffs: FramedVec<f32>, hop_size: usize) -> FramedVec<f32> {
+fn correct_phase_diffs(phase_diffs: &FramedVec<f32>, hop_size: usize) -> FramedVec<f32> {
     // We remove the expected phase difference due to natural accumulation
     // We also wrap back to [-π, π]
-    let mut corrected_phase_diffs: Vec<f32> = Vec::with_capacity(phase_diffs.len());
+    let mut corrected_phase_diffs: FramedVec<f32> = FramedVec::with_capacity_like(phase_diffs);
 
-    for diff in phase_diffs.iter() {
+    let number_of_frames = phase_diffs.number_of_frames();
+
+    for i in 0..number_of_frames {
+        let diff = phase_diffs.frame(i).expect("frame index out of bounds");
         let corrected_diff: Vec<f32> = diff
             .iter()
             .enumerate()
@@ -219,22 +240,22 @@ fn correct_phase_diffs(phase_diffs: FramedVec<f32>, hop_size: usize) -> FramedVe
                 phase_vocoder_helpers::modulo(observed - expected + PI, 2.0 * PI) - PI
             })
             .collect();
-        for c_diff in corrected_diff {
-            corrected_phase_diffs.push(c_diff);
-        }
+        corrected_phase_diffs.extend_from_slice(&corrected_diff);
     }
 
-    FramedVec::new(
-        corrected_phase_diffs,
-        phase_diffs.frame_size(),
-        phase_diffs.frame_size(),
-    )
+    return corrected_phase_diffs;
 }
 
 // gets the true frequency bins from the given bins and the phase differences
-fn get_true_frequency(phase_differences: FramedVec<f32>, hop_size: usize) -> FramedVec<f32> {
-    let mut true_frequencies: Vec<f32> = Vec::with_capacity(phase_differences.len());
-    for diff in phase_differences.iter() {
+fn get_true_frequency(phase_differences: &FramedVec<f32>, hop_size: usize) -> FramedVec<f32> {
+    let mut true_frequencies: FramedVec<f32> = FramedVec::with_capacity_like(phase_differences);
+
+    let number_of_frames = phase_differences.number_of_frames();
+
+    for i in 0..number_of_frames {
+        let diff = phase_differences
+            .frame(i)
+            .expect("frame index out of bounds");
         let true_freq_frame: Vec<f32> = diff
             .iter()
             .enumerate()
@@ -243,33 +264,31 @@ fn get_true_frequency(phase_differences: FramedVec<f32>, hop_size: usize) -> Fra
                 bin_freq + pd / (hop_size as f32)
             })
             .collect();
-        for true_freq in true_freq_frame {
-            true_frequencies.push(true_freq);
-        }
+        true_frequencies.extend_from_slice(&true_freq_frame);
     }
 
-    FramedVec::new(
-        true_frequencies,
-        phase_differences.frame_size(),
-        phase_differences.frame_size(),
-    )
+    return true_frequencies;
 }
 
-fn get_cumulative_phases(true_frequencies: FramedVec<f32>, hop_out: usize) -> FramedVec<f32> {
-    let mut final_phases: FramedVec<f32> =
-        FramedVec::new_empty_non_overlapped(true_frequencies.len(), true_frequencies.frame_size());
+fn get_cumulative_phases(true_frequencies: &FramedVec<f32>, hop_out: usize) -> FramedVec<f32> {
+    let mut final_phases: FramedVec<f32> = FramedVec::with_capacity_like(true_frequencies);
 
-    for freq_frame in true_frequencies.iter() {
-        let phase_diff_frame: Vec<f32> = freq_frame.iter().map(|f| f * (hop_out as f32)).collect();
-        let default: Vec<f32> = vec![0.; phase_diff_frame.len()];
-        // first frame no longer needs to be done manually
-        let prev_frame = final_phases.iter().last().unwrap_or(&default);
+    let number_of_frames = true_frequencies.number_of_frames();
+
+    for i in 0..number_of_frames {
+        let freq_frame: Vec<f32> = true_frequencies
+            .frame(i)
+            .expect("frame index out of bounds")
+            .iter()
+            .map(|f| f * (hop_out as f32))
+            .collect();
+        let default: Vec<f32> = vec![0.; freq_frame.len()];
+        let previous_frame = final_phases.frame(i.saturating_sub(1)).unwrap_or(&default);
         let cumulative_phase_frame =
-            phase_vocoder_helpers::elementwise_add(prev_frame, &phase_diff_frame);
-        final_phases.push(cumulative_phase_frame);
-        // TODO I'm updating indices every iteration -- this can't be efficient! Try to optimise
-        final_phases.update_indices();
+            phase_vocoder_helpers::elementwise_add(previous_frame, &freq_frame);
+        final_phases.extend_from_slice(&cumulative_phase_frame);
     }
+
     return final_phases;
 }
 
@@ -278,64 +297,60 @@ fn get_cumulative_phases(true_frequencies: FramedVec<f32>, hop_out: usize) -> Fr
 // Section 3: Synthesis
 
 fn inverse_fft(fft_spectrum: &FramedVec<Complex<f32>>, phases: &FramedVec<f32>) -> FramedVec<f32> {
-    let mut output_frames: FramedVec<f32> =
-        FramedVec::new_empty_non_overlapped(fft_spectrum.len(), fft_spectrum.frame_size());
-    for (fft_frame, phase_frame) in fft_spectrum.iter().zip(phases.iter()) {
+    let mut output_frames: FramedVec<f32> = FramedVec::with_capacity_like(phases);
+
+    let number_of_frames = fft_spectrum.number_of_frames();
+    let ifft_engine = Radix4::new(fft_spectrum.frame_size(), FftDirection::Inverse);
+
+    for i in 0..number_of_frames {
+        let fft_frame = fft_spectrum.frame(i).expect("frame index out of bounds");
+        let phase_frame = phases.frame(i).expect("frame index out of bounds");
         let rotated_frame = phase_vocoder_helpers::produce_output_frame(fft_frame, phase_frame);
-        let output_frame: Vec<f32> = apply_fft_to_frame(&rotated_frame, FftDirection::Inverse)
+        let output_frame: Vec<f32> = apply_fft_to_frame(&rotated_frame, &ifft_engine)
             // take the real part and normalise
             .iter()
             .map(|c| c.re / (rotated_frame.len() as f32))
             .collect();
-        output_frames.push(output_frame);
+        output_frames.extend_from_slice(&output_frame);
     }
-    output_frames.update_indices();
     return output_frames;
 }
 
 // TODO I expect this function will need further optimising
 fn overlap_add_frames(
-    output_frames: FramedVec<f32>,
+    output_frames: &FramedVec<f32>,
     hop_out: usize,
     apply_window: bool,
 ) -> Vec<f32> {
-    let mut raw_frames: FramedVec<f32> =
-        FramedVec::new_empty_non_overlapped(output_frames.len(), output_frames.frame_size());
+    let mut raw_frames: FramedVec<f32> = FramedVec::with_capacity_like(output_frames);
     println!("frame size is {}", output_frames.frame_size());
 
-    // apply window to each frame
-    for frame in output_frames.iter() {
+    let window = generate_hanning_window(output_frames.frame_size(), hop_out, true);
+
+    let number_of_frames = output_frames.number_of_frames();
+
+    for i in 0..number_of_frames {
+        let frame = output_frames.frame(i).expect("frame index out of bounds");
         let output_frame = if apply_window {
-            apply_hanning_window(&frame, hop_out)
+            apply_hanning_window(&frame, &window)
         } else {
             frame.to_vec()
         };
-        raw_frames.push(output_frame);
+        raw_frames.extend_from_slice(&output_frame);
     }
-    raw_frames.update_indices();
-    println!("raw frames length is {}", raw_frames.len());
 
     // set up the vectors
     let frame_length = raw_frames.frame_size();
     let new_waveform_size = (raw_frames.number_of_frames() - 1) * hop_out + frame_length;
     let mut overlapped_waveform: Vec<f32> = vec![0.; new_waveform_size];
 
-    // first iteration manually
-    let range = 0..frame_length;
-    let default = vec![0.; frame_length];
-    let mut frame_iterator = raw_frames.iter();
-    let first_frame = frame_iterator.nth(0).unwrap_or(&default);
-    let overlap_slice =
-        phase_vocoder_helpers::elementwise_add(&overlapped_waveform[range.clone()], first_frame);
-    let _ = overlapped_waveform.splice(range, overlap_slice);
-
-    // overlap add by hop_out hop size
-    for (i, frame) in frame_iterator.enumerate() {
-        let time_index = (i + 1) * hop_out;
-        let range = time_index..(time_index + frame_length);
+    for i in 0..number_of_frames {
+        let start = i * hop_out;
+        let end = start + frame_length;
+        let frame = raw_frames.frame(i).expect("frame index out of bounds");
         let overlap_slice =
-            phase_vocoder_helpers::elementwise_add(&overlapped_waveform[range.clone()], frame);
-        let _ = overlapped_waveform.splice(range, overlap_slice);
+            phase_vocoder_helpers::elementwise_add(&overlapped_waveform[start..end], &frame);
+        let _ = overlapped_waveform.splice(start..end, overlap_slice);
     }
 
     return overlapped_waveform;
@@ -436,7 +451,8 @@ mod tests {
             vec![5., 0., 0.],
         ];
         let frames = generate_frames(input, 3, 1);
-        for (i, frame) in frames.iter().enumerate() {
+        for i in 0..frames.number_of_frames() {
+            let frame = frames.frame(i).expect("frame index out of bounds");
             assert_eq!(frame, output[i]);
         }
     }
@@ -447,7 +463,7 @@ mod tests {
             0.0, 0.11697778, 0.41317591, 0.75, 0.96984631, 0.96984631, 0.75, 0.41317591,
             0.11697778, 0.0,
         ];
-        let hanning_generated = generate_hanning_window(10);
+        let hanning_generated = generate_hanning_window(10, 1, false);
         for (v1, v2) in hanning_test.iter().zip(hanning_generated.iter()) {
             assert!(v1 - v2 < 0.0001);
         }
@@ -456,7 +472,8 @@ mod tests {
     #[test]
     fn fft_test() {
         let v: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 4.0, 3.0, 2.0];
-        let fft_vec: Vec<f32> = apply_fft_to_frame(&v, FftDirection::Forward)
+        let fft_engine = Radix4::new(8, FftDirection::Forward);
+        let fft_vec: Vec<f32> = apply_fft_to_frame(&v, &fft_engine)
             .iter()
             .map(|v| v.re)
             .collect();
@@ -494,7 +511,7 @@ mod tests {
     fn test_correct_phase_diffs() {
         let my_vec = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0];
         let input = FramedVec::new(my_vec.clone(), 3, 3);
-        let corrected_pds = correct_phase_diffs(input, 1).clone_vec();
+        let corrected_pds = correct_phase_diffs(&input, 1).clone_vec();
         let output_vec: Vec<f32> = my_vec
             .iter()
             .enumerate()
@@ -512,7 +529,7 @@ mod tests {
     fn test_true_frequency() {
         let input: Vec<f32> = vec![0., 1., 2., 3., 4., 5.];
         let phase_differences = FramedVec::new(input.clone(), 3, 3);
-        let true_freq = get_true_frequency(phase_differences, 2).clone_vec();
+        let true_freq = get_true_frequency(&phase_differences, 2).clone_vec();
         let output_vec: Vec<f32> = input
             .iter()
             .enumerate()
@@ -534,7 +551,7 @@ mod tests {
     fn test_cumulative_phases() {
         let input: Vec<f32> = vec![0., 1., 2., 3., 4., 5.];
         let true_freqs = FramedVec::new(input, 3, 3);
-        let cumulative_phases = get_cumulative_phases(true_freqs, 1).clone_vec();
+        let cumulative_phases = get_cumulative_phases(&true_freqs, 1).clone_vec();
         let output_vec: Vec<f32> = vec![0., 1., 2., 3., 5., 7.];
         for (x, y) in cumulative_phases.iter().zip(output_vec.iter()) {
             println!("x: {x}, y: {y}");
@@ -588,7 +605,7 @@ mod tests {
         let my_vec = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let input: FramedVec<f32> = FramedVec::new(my_vec, 3, 3);
         let output: Vec<f32> = vec![1.0, 6.0, 8.0, 6.0];
-        assert_eq!(output, overlap_add_frames(input, hop, false));
+        assert_eq!(output, overlap_add_frames(&input, hop, false));
     }
 
     // End Test Section 3
